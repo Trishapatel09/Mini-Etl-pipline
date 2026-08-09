3fa63ccbd91d35be644deb1fee6224b68d779ef1
Trisha patel <pateltrisha0109@gmail.com>
2026-08-09 16:09:12 +0530

    Added data transformation with pandas

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-import requests
+# import requests
 import json
+import pandas as pd
 
-url = "https://api.open-meteo.com/v1/forecast?latitude=23.2599&longitude=77.4126&current=temperature_2m,relative_humidity_2m,wind_speed_10m"
+# url = "https://api.open-meteo.com/v1/forecast?latitude=23.2599&longitude=77.4126&current=temperature_2m,relative_humidity_2m,wind_speed_10m"
 
-response = requests.get(url)
+# response = requests.get(url)
 
-data = response.json()
+# data = response.json()
 
-temperature = data["current"]["temperature_2m"]
-humidity = data["current"]["relative_humidity_2m"]
-wind_speed = data["current"]["wind_speed_10m"]
+# temperature = data["current"]["temperature_2m"]
+# humidity = data["current"]["relative_humidity_2m"]
+# wind_speed = data["current"]["wind_speed_10m"]
 
-print("Temperature:", temperature)
-print("Humidity:", humidity)
-print("Wind Speed:", wind_speed)
+# print("Temperature:", temperature)
+# print("Humidity:", humidity)
+# print("Wind Speed:", wind_speed)
 
-with open("raw_data.json", "w") as file:
-    json.dump(data,file,indent=4)
+# with open("raw_data.json", "w") as file:
+#     json.dump(data,file,indent=4)
+    
+
+
+with open("raw_data.json", "r") as file:
+    data = json.load(file)
+
+current_data = data["current"]
+clean_data = {
+    "time": current_data["time"],
+    "temperature": current_data["temperature_2m"],
+    "humidity": current_data["relative_humidity_2m"],
+    "wind_speed": current_data["wind_speed_10m"]
+}
+
+print(clean_data)
+
+df=pd.DataFrame([clean_data])
+
+print(df)
